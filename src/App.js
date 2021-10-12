20d4de9c6f1b744ea4c2898ee6230de1542c52b6
Lindsay Lopez <lopez95@berkeley.edu>
2021-10-11 18:46:59 -0700

    made items editable, but clicking resubmit is still funky

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     this.state = {
       task: {
         text: '',
-        id: uniqid()
+        id: uniqid(),
+        canEdit: false
       },
       tasks: []
     }
@@ -38,6 +39,83 @@ class App extends Component {
     });
   };
 
+  handleDelete = (e) => {
+    const id = e.target.id.split('-')[1];
+    this.setState((prevState) => {
+      let taskIndex = prevState.tasks.findIndex(task => task.id === id);
+      return {
+        tasks: (prevState.tasks.slice(0, taskIndex)).concat(prevState.tasks.slice(taskIndex + 1))
+      };
+
+    });
+  };
+
+  handleTaskChange = (e) => {
+    const { textContent } = e.target;
+    const id = e.target.id.split('-')[1];
+    this.setState((prevState) => {
+      const newTasks = prevState.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            text: textContent,
+          }
+        } else {
+          return task;
+        }
+      });
+      return {
+        tasks: newTasks,
+      }
+    });
+  };
+
+  handleTaskChangeEnd = (e) =>  {
+    const id = e.target.id.split('-')[1];
+    const text = document.getElementById(`text-${id}`);
+    text.contentEditable = false;
+    text.removeEventListener('input', this.handleTaskChange);
+    text.removeEventListener('blur', this.handleTaskChangeEnd);
+    this.setState((prevState) => {
+      const newTasks = prevState.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            canEdit: false,
+          }
+        } else {
+          return task;
+        }
+      });
+      return {
+        tasks: newTasks,
+      }
+    });
+  };
+
+  handleClick = (e) => {
+    const id = e.target.id.split('-')[1];
+    const text = document.getElementById(`text-${id}`);
+    text.contentEditable = true;
+    text.addEventListener('input', this.handleTaskChange);
+    text.addEventListener('blur', this.handleTaskChangeEnd);
+    this.setState((prevState) => {
+      const newTasks = prevState.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            canEdit: true,
+          }
+        } else {
+          return task;
+        }
+      });
+      return {
+        tasks: newTasks,
+      }
+    });
+  }
+
   render() {
     return (
       <div>
@@ -49,7 +127,11 @@ class App extends Component {
           />
           <button type="submit">Submit</button>
         </form>
-        <Overview tasks={this.state.tasks}/>
+        <Overview 
+          tasks={this.state.tasks} 
+          handleDelete={this.handleDelete}
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,8 +1,27 @@
 function Overview(props) {
-  const { tasks } = props;
+  const { tasks, handleDelete, handleClick } = props;
   return (
     <ul>
-      {tasks.map((task) => <li key={task.id}>{task.text}</li>)}
+      {tasks.map((task) => {
+        return (
+          <li key={task.id} style={{listStyle: 'none'}}>
+            <button 
+              id={`delete-${task.id}`}
+              style={{display: 'inline', margin: '10px'}}
+              onClick={handleDelete}
+            >X</button>
+            <p 
+              id={`text-${task.id}`}
+              style={{display: 'inline', margin: '10px'}} 
+            >{task.text}</p>
+            <button 
+              id={`edit-${task.id}`}
+              style={{display: 'inline', margin: '10px'}}
+              onClick={handleClick}
+            >{task.canEdit ? 'Resubmit' : 'Edit'}</button>
+          </li>
+        )
+      })}
     </ul>
     
   );
